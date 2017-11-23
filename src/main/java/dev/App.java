5b45404cadbd784ba814781c97f7849c33d9b074
myR3po <>
2017-11-23 15:30:36 +0100

    [TP3] Etape 2 - Filtering

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -8,12 +8,16 @@ import com.github.lalyos.jfiglet.FigletFont;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	String titre = ResourceBundle.getBundle("application").getString("titre");
-        String asciiArt1 = FigletFont.convertOneLine(titre);
-        System.out.println(asciiArt1);
-    }
+public class App {
+	public static final String TITRE = "titre";
+	public static final String ENVIRONNEMENT = "environnement";
+	
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString(App.TITRE);
+		String asciiArt1 = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt1);
+
+		String environnement = ResourceBundle.getBundle("application").getString(App.ENVIRONNEMENT);
+		System.out.println("Environnement : " + environnement);
+	}
 }
